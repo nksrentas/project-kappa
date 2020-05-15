85a5200452e23adc77b45a4e39c30e0381bfe95d
satner <cse2343@gmail.com>
2020-05-15 17:50:00 +0300

    Navigation bar style

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -6,10 +6,12 @@ window.addEventListener("load", (event) => {
 
   navHamburger.addEventListener("click", (event) => {
     if (!isClicked) {
+      navHamburger.classList.add("nav__hamburger--active");
       navBar.classList.add("nav__bar--drop-menu");
       hamburgerSvgBtn.classList.add("hamburger--cross");
       isClicked = true;
     } else {
+      navHamburger.classList.remove("nav__hamburger--active");
       navBar.classList.remove("nav__bar--drop-menu");
       hamburgerSvgBtn.classList.remove("hamburger--cross");
       isClicked = false;
